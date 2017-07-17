8b5d65ba644ed7934273532f329d2b62eee090ee
Kelvin Lam <klam@networkten.com.au>
2017-07-17 10:54:15 +1000

    Initial commit for Xcode

---
+++ nsx-watcher/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  nsx-watcher
+//
+//  Created by Kelvin Lam on 17/7/17.
+//  Copyright © 2017 Henshin Soft Pty Ltd. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
